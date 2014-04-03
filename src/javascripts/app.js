48c75460a464f28b5f2123972a952944dfed1912
Joshua <josh.u.war@gmail.com>
2014-04-03 20:47:41 +0100

    corrected javascript and scss to make slide controls work

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -1,19 +1,5 @@
 jQuery(document).ready(function($) {
-
-
-	// set the wrap to have a class of 'slidePos-x' where x is the id of the link clicked
-
-	slideposition = function(slidepos) {
-		$('#' + slidepos).click(function() {
-			$(this).parents('.wrap').addClass('slidePos-' + slidepos);
-		})
-	};
-
-	// get the id of the link clicked and send it to slideposition func
-
 	$('a').click(function() {
-		slideposition(this.id);
+		$(this).parents('.wrap').attr('class', 'wrap slidePos-' + this.id);
 	});
-	
-
 });

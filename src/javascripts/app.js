9340051733c56cd7523d54597ec3f950b04c8780
Joshua <josh.u.war@gmail.com>
2014-04-03 19:08:07 +0100

    adding simple nav to control panes

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -1,6 +1,19 @@
 jQuery(document).ready(function($) {
-	$('.handleBar').click(function() {
-		console.log("boo");
-		$(this).parents('.wrap').addClass('slidePos-000');
+
+
+	// set the wrap to have a class of 'slidePos-x' where x is the id of the link clicked
+
+	slideposition = function(slidepos) {
+		$('#' + slidepos).click(function() {
+			$(this).parents('.wrap')addClass('slidePos-' + slidepos);
+		})
+	};
+
+	// get the id of the link clicked and send it to slideposition func
+
+	$('a').click(function() {
+		slideposition($(this).id());
 	});
+	
+
 });

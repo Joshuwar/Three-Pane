48c9b808d35ca9b8d608e1368ec7f49fdafddae3
Joshua <josh.u.war@gmail.com>
2014-04-08 15:51:52 +0100

    adds new 3 pane interface demo

--- Example 2/src/javascripts/app.js
+++ Example 2/src/javascripts/app.js
@@ -2,4 +2,7 @@ jQuery(document).ready(function($) {
 	$('a').click(function() {
 		$(this).parents('.wrap').attr('class', 'wrap slidePos-' + this.id);
 	});
+	$('a#notificationFlyout').click(function() {
+		$(this).parents('.wrap').addClass('flyout');
+	});
 });

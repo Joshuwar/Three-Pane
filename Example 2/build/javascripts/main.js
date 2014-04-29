5227a6ddf37276855f48a2a76f9e5e57c6b68ee3
Joshua <josh.u.war@gmail.com>
2014-04-29 16:47:25 +0100

    adds comments to app.js functions, removes unnecessary .closed class from resultBlock divs in html

--- Example 2/build/javascripts/main.js
+++ Example 2/build/javascripts/main.js
@@ -594,47 +594,50 @@ if ( window.jQuery || window.Zepto ) {
   })( window.jQuery || window.Zepto )
 }
 jQuery(document).ready(function($) {
+
+
+	// controls which pane is visible
 	$('a.paneControl').click(function() {
 		$(this).parents('.wrap').attr('class', 'wrap slidePos-' + this.id);
 		mySwipe.slide($(this).parent().index(), 600);
 		//alert($(this).index());
-
 	});
+
+	// toggles tool overlay
 	$('a.overlayLink, a.overlayClose').click(function() {
 		$(this).parents('.wrap').toggleClass( "overlayShown" );
 	});
+
+	// toggles  resultBlock display 
 	$('a.viewMore').click(function() {
 		$(this).parents('.resultBlock').toggleClass( "open" );
 		//$(this).text("View Less");
 	});
-	$('a.cardControls').click(function() {
+
+	// opens/closes interface cards
+	$('a.expand, a.collapse').click(function() {
 		$(this).parents('.card').toggleClass( "closed" );
 	});
+
+	// show/hide contextual nav ribbon
 	$('a.hideNav').click(function() {
 		$(this).parents('.contextualNavContainer').toggleClass( "closed" );
-		/*
-		This func changes the text of the contextual nav, but that has been superceded by css rotation
-		if($(this).parents('.contextualNavContainer').hasClass("closed")) {
-			$(this).html("&#9658;");
-		} else {
-			$(this).html("&#9668;");
-		}*/
 	});
-	$('.iconLink a').click(function() {
 
+	// show/hide top-right flyout menus
+	$('.iconLink a').click(function() {
 		var $wrap = $(this).parents('.wrap');
 		$(this).closest('.iconLink').siblings().children('a').each(function() {
 			$wrap.removeClass(this.id);
 		});
-
 		$wrap.toggleClass(this.id);
-		
 	});
 
+	// initialise swipe.js controls
 	window.mySwipe = new Swipe(document.getElementById('slider'), {
       continuous: false,
 		callback: function(index, elem) {
-//			alert($(elem).data('navid'));
+			// alert($(elem).data('navid'));
 			$('.wrap').attr('class', 'wrap slidePos-' + $(elem).data('navid'));
 		},
 	});
